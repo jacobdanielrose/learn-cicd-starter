aab16004ad9957a0fe2068edf8bdead98ff47111
Jacob Rose <jacobdanielrose@protonmail.com>
2025-09-11 15:06:04 +0200

    intentional failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,7 +10,9 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorization")
+	// authHeader := headers.Get("Authorization")
+	authHeader := headers.Get("sdfthorization")
+
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
